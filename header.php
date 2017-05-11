74c77530535a29ba52948e6322b49f8a27bc6587
sinaiyuc <sinai.yunker@gmail.com>
2017-05-11 14:13:31 -0700

    Added side bar on home page plus added bootstrap

--- header.php
+++ header.php
@@ -15,35 +15,9 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="stylesheet" href="<?php bloginfo('template_url');?>/fonts/icomoon/style.css">
 
 <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'portshowlio17' ); ?></a>
-
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
-
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'portshowlio17' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
-
-	<div id="content" class="site-content">
--- template-home.php
+++ template-home.php
@@ -1,9 +1,69 @@
-<?php 
-/* Template Name:  Home */ 
+<?php
+/* Template Name:  Home */
 ?>
 
 <?php get_header(); ?>
 
-Home Page Template
+<div class="sidebar">
+
+    <div class='Logo'>
+        <span class="icon-port-logo_black-nockout-ps-full"></span>
+    </div>
+
+    <div class='hr'></div>
+
+    <span id='workLink' class='filterMain'>
+        <?php the_field('student_link'); ?>
+    </span>
+    <span id='studentLink' class='filterMain'>
+        <?php the_field('work_link'); ?>
+    </span>
+
+    <div class='hr'></div>
+
+    <div class='dates'>
+        <?php if( have_rows('info') ): ?>
+            <?php while ( have_rows('info') ) : the_row(); ?>
+                <?php if( get_row_layout() == 'night_details' ): ?>
+                    <div class='oneNight'>
+                        <span class='nightType'>
+                            <?php the_sub_field('night_type'); ?>
+                        </span>
+                        <span class='date'>
+                            <?php the_sub_field('date'); ?>
+                        </span>
+                        <span class='time'>
+                            <?php the_sub_field('time'); ?>
+                        </span>
+                    </div>
+                <?php elseif( get_row_layout() == 'download' ): ?>
+                <?php endif; ?>
+            <?php endwhile; ?>
+        <?php else : ?>
+           <!-- nothing found -->
+        <?php endif; ?>
+
+        <span class='eventLink'>Event Details<span class='icon-arrow'></span></span>
+
+    </div>
+
+    <div class='hr'></div>
+
+    <div class='socialLinks'>
+        <span class='icon-facebook socialbox'></span>
+        <span class='icon-instagram socialbox'></span>
+        <span class='icon-twitter socialbox'></span>
+    </div>
+
+
+
+</div>
+
+<div class="container">
+
+
+
+</div>
+
 
 <?php get_footer(); ?>
