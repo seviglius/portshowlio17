5fe2cc6d4f273070eabe25829cc74feccf8b2098
kaitlync3 <kaitlyn.castellow@gmail.com>
2017-05-16 12:08:14 -0700

    Changes to student pg & css
    cleaned up student php and added css

--- single-design.php
+++ single-design.php
@@ -44,151 +44,145 @@
 
 <div class="container">
 	<div class="row">
-		<div class="col-xl-6 col-lg-8 col-md-12">
 
-<?php if( have_rows('projects') ): ?>
-	<?php while ( have_rows('projects') ) : the_row(); ?>
+				<?php if( have_rows('projects') ): ?>
+					<?php while ( have_rows('projects') ) : the_row(); ?>
+						<div class="col-xl-6 col-lg-8 col-md-12">
+	        		<?php if( get_row_layout() == 'project' ): ?>
 
-        <?php if( get_row_layout() == 'project' ): ?>
+	        			<h3 class="projectTitle"><?php the_sub_field('project_title'); ?></h3>
 
-        		<h3 class="projectTitle"><?php the_sub_field('project_title'); ?></h3>
-
-				<!-- Figure out how to output project types with commas -->
-        		<p class="category"><?php the_sub_field('project_type'); ?></p>
-
-
-				<?php $post_objects = get_field('collaborators'); if( $post_objects ): ?>
-					<?php foreach( $post_objects as $post): // variable must be called $post (IMPORTANT) ?>
-				        <?php setup_postdata($post); ?>
-				        <a class="collaborators" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-				    <?php endforeach; ?>
-				    </p>
-				    <?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
-				<?php endif; ?>
+								<!-- Figure out how to output project types with commas -->
+	        			<p class="category"><?php the_sub_field('project_type'); ?></p>
 
 
-<hr>
-
-				<p class="projectDescription"><?php the_sub_field('project_description'); ?></p>
-
-</div><!--col 6 close -->
-	<div class="col-lg-12 col-md-0">
-	</div>
-
-
-
-				<?php if( have_rows('project_images') ): ?>
-					<?php while ( have_rows('project_images') ) : the_row(); ?>
+								<?php $post_objects = get_field('collaborators'); if( $post_objects ): ?>
+									<?php foreach( $post_objects as $post): // variable must be called $post (IMPORTANT) ?>
+					        	<?php setup_postdata($post); ?>
+					        	<a class="collaborators" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+					    		<?php endforeach; ?>
+					    	</p>
+					    	<?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
+							<?php endif; ?>
 
 
+							<hr>
+
+							<p class="projectDescription"><?php the_sub_field('project_description'); ?></p>
+
+						</div><!--col 6 close -->
+						<div class="col-lg-12 col-md-0">
+						</div>
+
+
+
+						<?php if( have_rows('project_images') ): ?>
+							<?php while ( have_rows('project_images') ) : the_row(); ?>
+
+								<!--3 COLUMN PORTRAIT -->
 				        <?php if( get_row_layout() == 'portrait_3_column' ): ?>
 
+									<?php if( have_rows('images') ): ?>
+										<?php  while( have_rows('images') ) : the_row(); ?>
 
-								<?php if( have_rows('images') ): ?>
+										 <div class="col-md-4 imageGrid">
+											 <?php $imageurl = get_sub_field('image'); ?>
+									    	<img src="<?php echo $imageurl; ?>" />
+											</div><!--col 4 close -->
 
-								<?php  while( have_rows('images') ) : the_row(); ?>
+										<?php endwhile; ?>
+									<?php endif; ?>
 
-
-
-									   <?php $imageurl = get_sub_field('image'); ?>
-								<div class="col-md-4 imageGrid">
-									    <img src="<?php echo $imageurl; ?>" />
-
-										</div><!--col 4 close -->
-
-									<?php endwhile; ?>
-
-								<?php endif; ?>
-
-
-
+								<!--2 COLUMN PORTRAIT -->
 				        <?php elseif( get_row_layout() == 'portrait_2_column' ): ?>
-
 				        	<?php if(get_sub_field('images')): ?>
+										<?php while(has_sub_field('images')): ?>
 
-									<?php while(has_sub_field('images')): ?>
+											<?php /*
+												$img = get_sub_field('image');
+												$img_title = $img['title'];
+												$img_alt = $img['alt'];
+												$img_caption = $img['caption'];
+												$img_desc = $img['description'];
+												$img_url = $img['url'];
+												$img_thumb = $img['sizes']['medium']; */
+											?>
+
+											<div class="col-md-6 imageGrid">
+												<?php $imageurl = get_sub_field('image'); ?>
+												<img src="<?php echo $imageurl; ?>" />
+											</div>
+
+										<?php endwhile; ?>
+									<?php endif; ?>
+
+								<!--1 COLUMN PORTRAIT -->
+								<?php elseif( get_row_layout() == 'portrait_1_column' ): ?>
+
+									<?php
+										$img = get_sub_field('image');
+										$img_title = $img['title'];
+										$img_alt = $img['alt'];
+										$img_caption = $img['caption'];
+										$img_desc = $img['description'];
+										$img_url = $img['url'];
+										$img_thumb = $img['sizes']['medium'];
+									?>
+									<div class="col-md-3"></div>
+									<div class="col-md-6 col-md-offset-3 imageGrid">
+										<?php $imageurl = get_sub_field('image'); ?>
+										<img src="<?php echo $imageurl; ?>" />
+									</div>
+									<div class="col-md-3"></div>
+
+								<!--Landscape -->
+				        <?php elseif( get_row_layout() == 'landscape_image' ): ?>
+				        	<?php /*
+										$img = get_sub_field('image');
+										$img_title = $img['title'];
+										$img_alt = $img['alt'];
+										$img_caption = $img['caption'];
+										$img_desc = $img['description'];
+										$img_url = $img['url'];
+										$img_thumb = $img['sizes']['medium']; */
+									?>
 
 
-										<?php /*
-											$img = get_sub_field('image');
-											$img_title = $img['title'];
-											$img_alt = $img['alt'];
-											$img_caption = $img['caption'];
-											$img_desc = $img['description'];
-											$img_url = $img['url'];
-											$img_thumb = $img['sizes']['medium']; */
-										?>
+						 		<div class="col-md-12 imageGrid">
+									<?php $imageurl = get_sub_field('image'); ?>
+									 <img src="<?php echo $imageurl; ?>" />
+								 </div><!--col 12 close -->
 
-
-
-	<div class="col-md-6 imageGrid">
-		<img src="<?php echo $imageurl; ?>" />
+								<!--Video -->
+				        <?php elseif( get_row_layout() == 'video' ): ?>
+									<div class="col-md-12 imageGrid video">
+				        		<?php the_sub_field('video'); ?>
+			
 									</div>
 
-									<?php endwhile; ?>
-
-								<?php endif; ?>
-
-
-
-						<?php elseif( get_row_layout() == 'portrait_1_column' ): ?>
-
-							<?php
-								$img = get_sub_field('image');
-								$img_title = $img['title'];
-								$img_alt = $img['alt'];
-								$img_caption = $img['caption'];
-								$img_desc = $img['description'];
-								$img_url = $img['url'];
-								$img_thumb = $img['sizes']['medium'];
-							?>
-<div class="col-md-3"></div>
-							<div class="col-md-6 col-md-offset-3 imageGrid">
-								<img src="<?php echo $imageurl; ?>" />
-															</div>
-<div class="col-md-3"></div>
-
-				        <?php elseif( get_row_layout() == 'landscape_image' ): ?>
-				        	<?php
-								$img = get_sub_field('image');
-								$img_title = $img['title'];
-								$img_alt = $img['alt'];
-								$img_caption = $img['caption'];
-								$img_desc = $img['description'];
-								$img_url = $img['url'];
-								$img_thumb = $img['sizes']['medium'];
-							?>
-
-							<div class="col-md-12 imageGrid">
-								<img src="<?php echo $imageurl; ?>" />
-															</div>
-
-							<div class="col-md-12 imageGrid">
-				        <?php elseif( get_row_layout() == 'video' ): ?>]
-				        	<?php the_sub_field('video'); ?>
-								</div>
-
 				        <?php endif; ?>
-				    <?php endwhile; ?>
+				    	<?php endwhile; ?>
 
-				<?php else : ?>
-				   <!-- nothing found -->
-				<?php endif; ?>
-
-
+						<?php else : ?>
+				   		<!-- nothing found -->
+						<?php endif; ?>
 
 
 
-        <?php elseif( get_row_layout() == 'download' ): ?>
-
-        <?php endif; ?>
-    <?php endwhile; ?>
-
-<?php else : ?>
-   <!-- nothing found -->
-<?php endif; ?>
 
 
+        	<?php elseif( get_row_layout() == 'download' ): ?>
+
+        	<?php endif; ?>
+    		<?php endwhile; ?>
+
+			<?php else : ?>
+   		<!-- nothing found -->
+			<?php endif; ?>
+
+	</div>
+</div>
+</div>
 
 
 
